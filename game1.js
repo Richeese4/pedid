6c4dd01095a934acbfeb269ec0b2cc455afc1ec3
Richeese4 <78680000+Richeese4@users.noreply.github.com>
2024-05-08 11:56:46 +0700

    Update

---
+++ game1.js
@@ -0,0 +1,22 @@
+ var api = '74e98956be55214'
+  $('#id').on('change', function () {
+   var id = $(this).val();
+   $.ajax({
+    method: "GET",
+    url: "https://api-rekening.my.id/trueid/game/" + game + "/?id=" + id + "&key=" + api,
+    beforeSend: function () {
+     $('#cek').val('Sedang Mengecek ID Player');
+    },
+    success: function (response) {
+     if (response.hasOwnProperty('error_msg')) {
+      $('#cek').val(response.error_msg);
+      $('#nick').text('Gagal Cek!');
+     } else {
+      $('#cek').val(response.nickname);
+      $('#nick').text(response.nickname);
+      $('#linkBeli').show();
+      $('#spaces').show();
+     }
+    }
+   });
+  });
